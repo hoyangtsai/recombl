02047b50ddc1f4273d1d6b66601b2fb4b45015bb
keithytsai <keithytsai@tencent.com>
2017-03-20 12:26:51 +0800

    Fix webpack-dev-server cannot find js modules

--- bin/index.js
+++ bin/index.js
@@ -1,21 +1,15 @@
 #! /usr/bin/env node
 
-require('babel-register');
-
 const argv = require('minimist')(process.argv.slice(2));
 
 if (argv['add'] || argv['_'].includes('add')) {
   console.log('add template');
   // require('../lib/add');
-}
-
-if (argv['watch'] || argv['w'] || argv['_'].includes('dev')) {
+} else if (argv['watch'] || argv['w'] || argv['_'].includes('dev')) {
   // console.log('start dev');
   require('../lib/server');
-}
-
-if (argv['_'].includes('update')) {
+} else if (argv['_'].includes('update')) {
   console.log('update config');
+} else {
+  require('../lib/webpack');
 }
-
-require('../lib/server');
