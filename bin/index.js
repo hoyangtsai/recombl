54076823d5ad2ba1a611ff1f2e4dce0afd51ebbb
keithytsai <keithytsai@tencent.com>
2017-03-21 17:34:28 +0800

    Implement the first version of dev mode

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,18 @@
 #! /usr/bin/env node
 
+const path = require('path');
 const argv = require('minimist')(process.argv.slice(2));
 
-if (argv['add'] || argv['_'].includes('add')) {
+process.env.MODULE_PATH = path.resolve(__dirname, '../node_modules');
+
+if (argv['watch'] || argv['w'] || argv['_'].includes('dev')) {
+  process.env.DEV = true;
+  require('../lib/server');
+} else if (argv['add'] || argv['_'].includes('add')) {
   console.log('add template');
   // require('../lib/add');
-} else if (argv['watch'] || argv['w'] || argv['_'].includes('dev')) {
-  // console.log('start dev');
-  require('../lib/server');
-} else if (argv['_'].includes('update')) {
-  console.log('update config');
-} else {
-  require('../lib/webpack');
 }
+
+// else {
+//   require('../lib/webpack');
+// }
