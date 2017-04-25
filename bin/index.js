a31f4239364d278cf265c9f34e05be77269d0b8b
keithytsai <keithytsai@tencent.com>
2017-04-25 11:43:54 +0800

    Using process.env.NODE_ENV to switch development and production env

--- bin/index.js
+++ bin/index.js
@@ -8,11 +8,12 @@ process.env.DEV_DIR = '_tmp';
 process.env.PUBLISH_DIR = 'publish';
 
 if (argv['watch'] || argv['w'] || argv['_'].includes('dev')) {
-  process.env.DEV = true;
+  process.env.NODE_ENV = 'development';
   require('../lib/server');
 } else if (argv['new'] || argv['_'].includes('new')) {
   require('../lib/new');
 } else if ( argv['publish'] || argv['p']  || argv['_'].includes('publish') ) {
+  process.env.NODE_ENV = 'production';
   require('../lib/publish');
 } else if ( argv['init'] || argv['_'].includes('init') ) {
   require('../lib/init');
