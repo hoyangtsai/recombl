952bc0be5e2c20f2cd7a341e18750fad11e22731
Ho-Yang Tsai <hoyang.t@gmail.com>
2017-12-20 10:41:10 +0800

    minChunks default set false

--- assets/config/pageConfig.js
+++ assets/config/pageConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   entry: ["index"], //页面文件列表 Array or Object
   commonsChunk: {
     name: null, //公共js、样式文件名，默认common
-    minChunks: null, //至少几个文件出现才抽取公共
+    minChunks: false, //至少几个文件出现才抽取公共
     exclude: []
   },
   sprites: { //覆写 userConfg.js 雪碧图配置
